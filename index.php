6e4b622bf74304e365c966d4ea04d9dc015d8976
蒋松佑 <xavierjiang@123u.com>
2014-10-15 10:20:47 +0800

    增加google analysis

--- index.php
+++ index.php
@@ -113,6 +113,14 @@
 			}
 			requestAnimationFrame(frame);
 		</script>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-31137233-1', 'auto');
+			ga('send', 'pageview');
+		</script>
 		<!--[if lt IE 9]>
 		<script src="media/html5shiv.js"></script>
 		<![endif]-->
